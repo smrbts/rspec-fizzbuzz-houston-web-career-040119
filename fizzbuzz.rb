25340db36628de9773e70ddeee85c87eb156e8ca
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-20 03:14:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  else if int % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
-  else if int % 3 == 0
+  elsif int % 3 == 0
     return "Fizz"
   end
 end
