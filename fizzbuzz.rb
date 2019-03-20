19cd6dcc2fddb02a8ede3125e57729e2e90b645b
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-20 03:14:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  else if int % 5 == 0
+    return "Buzz"
+  else if int % 3 == 0
+    return "Fizz"
+  end
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
